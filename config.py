62719686c7f9cc8d5c2b0cafdbafdd5b97e49290
Geoffrey <gbhand@ucsd.edu>
2020-07-25 22:10:08 -1000

    Fixed Apple captive text

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 sites = {
 	"http://www.msftconnecttest.com/connecttest.txt": "Microsoft Connect Test",
-	"http://captive.apple.com": "Success"
+	"http://captive.apple.com": "<HTML><HEAD><TITLE>Success</TITLE></HEAD><BODY>Success</BODY></HTML>"
 	}
